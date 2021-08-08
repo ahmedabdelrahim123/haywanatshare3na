92a486f6974ca0a344aa3f651f1e4ad081c5887d
ahmed abdelrahim <aahmed.mabdelrahim60@gmail.com>
2021-08-08 17:26:55 +0200

    login

--- app/Http/Controllers/OurloginController.php
+++ app/Http/Controllers/OurloginController.php
@@ -1,13 +1,31 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class OurloginController extends Controller
 {
+
+
     public function index(){
         return view('our.ourlogin');
-
+       // return view('auth.ourlogin');
     } 
+    //my edit
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!auth()->attempt($request->only('email', 'password'), $request->remember)) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('dashboard');
+    }
+    //
 }
