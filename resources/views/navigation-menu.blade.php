6a0a09ce108b2c0fc99ab563248d6428475ead07
tonyfayez <48352659+tonyfayez@users.noreply.github.com>
2021-08-09 18:56:04 +0200

    .

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -48,10 +48,10 @@
                                     </x-jet-dropdown-link>
 
                                     @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                                        <x-jet-dropdown-link href="{{ route('teams.create') }}">
-                                            {{ __('Create New Team') }}
-                                        </x-jet-dropdown-link>
-                                    @endcan
+                                    <x-jet-dropdown-link href="{{ route('teams.create') }}">
+                                        {{ __('Create New Team') }}
+                                    </x-jet-dropdown-link>
+                                @endcan
 
                                     <div class="border-t border-gray-100"></div>
 
